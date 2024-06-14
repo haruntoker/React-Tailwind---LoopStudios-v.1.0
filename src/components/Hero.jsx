ef95655c52372614723355055fdef75833c0ffc0
harun toker <haruntoker@icloud.com>
2024-06-14 14:19:17 +0300

    testimonial added.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,20 +1,21 @@
 import SubHero from "./SubHero";
-import SocialProof from "./SocialProof"
+import TestimonialHero from "./TestimonialHero";
 import Lottie from 'lottie-react';
-import Animation from '../assets/desktop/Animation - 1718353947790.json'
+import Animation from '../assets/desktop/Animation .json'
+
 
 export default function Hero() {
   return (
     <section id="hero" >
      {/* hero */}
-    
      <div className="flex flex-1 max-w-screen   mx-auto px-6 sm:mb-4">
         <div className="max-w-7xl text-center flex-1 mt-32 md:mb-32 p-4 font-sans text-2xl dark:text-white text-gray-900  md:p-10 md:m-32 md:mx-0 md:text-6xl rounded-2xl">
-         <h1 className="font-mono font-bold tracking-wide"> Impressive experience that deliver</h1>
-         <SocialProof />
+         <h1 className="font-mono font-bold tracking-wide py-4"> Impressive experience that deliver</h1>
+         <TestimonialHero />
         </div>
+        
       {/* animation start */}
-     <div className="hidden  md:flex flex-row-reverse  mt-20 inset-0 z-0 pointer-events-none">
+     <div className="hidden md:flex flex-row-reverse  mt-20 inset-0 z-0 pointer-events-none">
     <Lottie animationData={Animation} loop={true} />
     </div>
     {/* animation end */}
---
+++ src/components/TestimonialHero.jsx
@@ -0,0 +1,46 @@
+
+
+export default function TestimonialHero() {
+  return (
+    <div>
+      <section className="relative  overflow-hidden px-6 py-24 sm:py-32 lg:px-8">
+        <div className="absolute inset-0 -z-10 dark:text-white"/>
+        <div className="mx-auto max-w-2xl lg:max-w-4xl">
+  <img
+    className="mx-auto h-12"
+    src="https://cdn3.iconfinder.com/data/icons/eco-tech/512/09_Green_Technology.png"
+    alt="Tech Company Logo"
+  />
+  <figure className="mt-10">
+    <blockquote className="text-center text-xl font-semibold leading-8 text-gray-900 dark:text-gray-300 sm:text-2xl sm:leading-9">
+      <p>
+        “At our tech company, we are committed to pushing the boundaries of innovation. Our cutting-edge solutions empower businesses to thrive in a rapidly evolving digital landscape.”
+      </p>
+    </blockquote>
+    <figcaption className="mt-10">
+      <img
+        className="mx-auto h-15 w-12 rounded-full"
+        src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?q=80&w=3000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="CEO of Tech Company"
+      />
+      <div className="mt-4 flex items-center justify-center space-x-3 text-base">
+        <div className="font-semibold text-gray-900 dark:text-gray-300">Alex Johnson</div>
+        <svg
+          viewBox="0 0 2 2"
+          width={3}
+          height={3}
+          aria-hidden="true"
+          className="fill-gray-900"
+        >
+          <circle cx={1} cy={1} r={1} />
+        </svg>
+        <div className="text-gray-600 dark:text-gray-300">CEO of LoopStudios</div>
+      </div>
+    </figcaption>
+  </figure>
+</div>
+
+      </section>
+    </div>
+  );
+}
