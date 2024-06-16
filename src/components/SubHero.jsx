93254e3fa4ca205f3740f86d7be47bc0f343302b
harun toker <haruntoker@icloud.com>
2024-06-16 15:01:27 +0300

    change

--- src/components/SubHero.jsx
+++ src/components/SubHero.jsx
@@ -5,6 +5,7 @@ export default function SubHero() {
     <div>
         {/* <Content /> */}
         <section className="bg-white dark:bg-gray-900 ">
+            
     <div className="py-8 px-4 mx-auto max-w-screen-5xl text-center lg:py-24 lg:px-12 ">
         <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
             <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">New</span> <span className="text-sm font-medium">Exciting news! Discover our latest product release.</span> 
